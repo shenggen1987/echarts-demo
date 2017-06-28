0659327a8e58733df8bb1d9f31ec63d16db411cd
lishenggen <lishenggen@qjdchina.com>
2017-06-28 14:04:33 +0800

    dist

---
+++ src/components/Hello.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="hello">
+    <div id="main" style="width: 600px;height:400px;"></div>
+  </div>
+</template>
+
+<script>
+var echarts = require('echarts')
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  mounted () {
+    var myChart = echarts.init(document.getElementById('main'))
+    // 绘制图表
+    myChart.setOption({
+      title: { text: 'ECharts 入门示例' },
+      tooltip: {},
+      xAxis: {
+        data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+      },
+      yAxis: {},
+      series: [{
+        name: '销量',
+        type: 'bar',
+        data: [5, 20, 36, 10, 10, 20]
+      }]
+    })
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
