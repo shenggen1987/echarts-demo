511448afeeca38bc8af8f158490595eafb64c0e5
lishenggen <lishenggen@qjdchina.com>
2017-06-29 17:47:52 +0800

    dist

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script>
-var echarts = require('echarts')
+// var echarts = require('echarts')
 import { options } from '../lib/map.js'
 import { chinaJson } from '../lib/china.js'
 
@@ -22,29 +22,68 @@ export default {
     // 绘制图表
     myChart.setOption(options)
 
-    let currentIndex = -1
+    // var currentIndex = -1
+    // setInterval(function () {
+    //   var dataLen = options.series[0].data.length
+    //   console.log(currentIndex)
+    //   // 取消之前高亮的图形
+    //   myChart.dispatchAction({
+    //     type: 'downplay',
+    //     seriesIndex: 0,
+    //     dataIndex: currentIndex
+    //   })
+    //   currentIndex = (currentIndex + 1) % dataLen
+    //   // 高亮当前图形
+    //   myChart.dispatchAction({
+    //     type: 'highlight',
+    //     seriesIndex: 0,
+    //     dataIndex: currentIndex
+    //   })
+    //   // 显示 tooltip
+    //   myChart.dispatchAction({
+    //     type: 'showTip',
+    //     seriesIndex: 0,
+    //     dataIndex: currentIndex
+    //   })
+
+    //   // myChart.setOption({
+    //   //   series: [{
+    //   //     center: options.series[0].data[currentIndex].coord,
+    //   //     zoom: 4,
+    //   //     data: [
+    //   //         {name: options.series[0].data[currentIndex].name, selected: true}
+    //   //     ],
+    //   //     animationDurationUpdate: 1000,
+    //   //     animationEasingUpdate: 'cubicInOut'
+    //   //   }]
+    //   // })
+    // }, 1000)
+
+    var bmap = myChart.getModel().getComponent('bmap').getBMap()
+    bmap.addControl(new BMap.MapTypeControl())
+    var myIcon = new BMap.Icon('http://banana001.w221.mc-test.com/Content/img/marker.png', new BMap.Size(1, 1))
+    var arr = [
+      {
+        title: '美的空调',
+        x: 91.1865,
+        y: 30.1465
+      },
+      {
+        title: '亚士漆',
+        x: 116.0046,
+        y: 28.6633
+      }
+    ]
+    var currentIndex = -1
     setInterval(function () {
-      var dataLen = options.series[0].data.length
-      // 取消之前高亮的图形
-      myChart.dispatchAction({
-        type: 'downplay',
-        seriesIndex: 0,
-        dataIndex: currentIndex
-      })
+      var dataLen = arr.length
       currentIndex = (currentIndex + 1) % dataLen
-      // 高亮当前图形
-      myChart.dispatchAction({
-        type: 'highlight',
-        seriesIndex: 0,
-        dataIndex: currentIndex
-      })
-      // 显示 tooltip
-      myChart.dispatchAction({
-        type: 'showTip',
-        seriesIndex: 0,
-        dataIndex: currentIndex
-      })
-    }, 1000)
+      var dd = arr[currentIndex]
+      var marker1 = new BMap.Marker(new BMap.Point(dd.x, dd.y), { icon: myIcon })
+      bmap.addOverlay(marker1)
+      var infoWindow1 = new BMap.InfoWindow(dd.title + '<br>合同金额: 1210万<br>签约时间: 2017-06-28')
+      marker1.openInfoWindow(infoWindow1)
+    }, 2000)
   }
 }
 </script>
--- src/lib/map1.js
+++ src/lib/map1.js
@@ -116,16 +116,42 @@ var geoCoordMap = {
 };
 
 var BJData = [
-    [{name:'杭州'}, {name:'上海',value:95}],
-    [{name:'杭州'}, {name:'广州',value:90}],
-    [{name:'杭州'}, {name:'大连',value:80}],
-    [{name:'杭州'}, {name:'南宁',value:70}],
-    [{name:'杭州'}, {name:'南昌',value:60}],
-    [{name:'杭州'}, {name:'拉萨',value:50}],
-    [{name:'杭州'}, {name:'长春',value:40}],
-    [{name:'杭州'}, {name:'包头',value:30}],
-    [{name:'杭州'}, {name:'重庆',value:20}],
-    [{name:'杭州'}, {name:'常州',value:10}]
+    [{name:'北京'}, {name:'上海',value:95}],
+    [{name:'北京'}, {name:'广州',value:90}],
+    [{name:'北京'}, {name:'大连',value:80}],
+    [{name:'北京'}, {name:'南宁',value:70}],
+    [{name:'北京'}, {name:'南昌',value:60}],
+    [{name:'北京'}, {name:'拉萨',value:50}],
+    [{name:'北京'}, {name:'长春',value:40}],
+    [{name:'北京'}, {name:'包头',value:30}],
+    [{name:'北京'}, {name:'重庆',value:20}],
+    [{name:'北京'}, {name:'常州',value:10}]
+];
+
+var SHData = [
+    [{name:'上海'},{name:'包头',value:95}],
+    [{name:'上海'},{name:'昆明',value:90}],
+    [{name:'上海'},{name:'广州',value:80}],
+    [{name:'上海'},{name:'郑州',value:70}],
+    [{name:'上海'},{name:'长春',value:60}],
+    [{name:'上海'},{name:'重庆',value:50}],
+    [{name:'上海'},{name:'长沙',value:40}],
+    [{name:'上海'},{name:'北京',value:30}],
+    [{name:'上海'},{name:'丹东',value:20}],
+    [{name:'上海'},{name:'大连',value:10}]
+];
+
+var GZData = [
+    [{name:'广州'},{name:'福州',value:95}],
+    [{name:'广州'},{name:'太原',value:90}],
+    [{name:'广州'},{name:'长春',value:80}],
+    [{name:'广州'},{name:'重庆',value:70}],
+    [{name:'广州'},{name:'西安',value:60}],
+    [{name:'广州'},{name:'成都',value:50}],
+    [{name:'广州'},{name:'常州',value:40}],
+    [{name:'广州'},{name:'北京',value:30}],
+    [{name:'广州'},{name:'北海',value:20}],
+    [{name:'广州'},{name:'海口',value:10}]
 ];
 
 var planePath = 'path://M1705.06,1318.313v-89.254l-319.9-221.799l0.073-208.063c0.521-84.662-26.629-121.796-63.961-121.491c-37.332-0.305-64.482,36.829-63.961,121.491l0.073,208.063l-319.9,221.799v89.254l330.343-157.288l12.238,241.308l-134.449,92.931l0.531,42.034l175.125-42.917l175.125,42.917l0.531-42.034l-134.449-92.931l12.238-241.308L1705.06,1318.313z';
@@ -149,90 +175,90 @@ var convertData = function (data) {
 
 var color = ['#a6c84c', '#ffa022', '#46bee9'];
 var series = [];
-[['杭州', BJData]].forEach(function (item, i) {
+[['北京', BJData], ['上海', SHData], ['广州', GZData]].forEach(function (item, i) {
     series.push({
-            name: item[0] + ' Top10',
-            type: 'lines',
-            coordinateSystem: 'bmap',
-            zlevel: 1,
-            effect: {
-                show: true,
-                period: 6,
-                trailLength: 0.7,
-                color: '#fff',
-                symbolSize: 3
-            },
-            lineStyle: {
-                normal: {
-                    color: color[i],
-                    width: 0,
-                    curveness: 0.2
-                }
-            },
-            data: convertData(item[1])
+        name: item[0] + ' Top10',
+        type: 'lines',
+        zlevel: 1,
+        effect: {
+            show: true,
+            period: 6,
+            trailLength: 0.7,
+            color: '#fff',
+            symbolSize: 3
         },
-        {
-            name: item[0] + ' Top10',
-            type: 'lines',
-            coordinateSystem: 'bmap',
-            zlevel: 2,
-            effect: {
-                show: true,
-                period: 6,
-                trailLength: 0,
-                symbol: planePath,
-                symbolSize: 15
-            },
-            lineStyle: {
-                normal: {
-                    color: color[i],
-                    width: 1,
-                    opacity: 0.4,
-                    curveness: 0.2
-                }
-            },
-            data: convertData(item[1])
+        lineStyle: {
+            normal: {
+                color: color[i],
+                width: 0,
+                curveness: 0.2
+            }
         },
-        {
-            name: item[0] + ' Top10',
-            type: 'effectScatter',
-            coordinateSystem: 'bmap',
-            zlevel: 2,
-            rippleEffect: {
-                brushType: 'stroke'
-            },
-            label: {
-                normal: {
-                    show: true,
-                    position: 'right',
-                    formatter: '{b}'
-                }
-            },
-            symbolSize: function (val) {
-                return val[2] / 8;
-            },
-            itemStyle: {
-                normal: {
-                    color: color[i]
-                }
-            },
-            data: item[1].map(function (dataItem) {
-                return {
-                    name: dataItem[1].name,
-                    value: geoCoordMap[dataItem[1].name].concat([dataItem[1].value])
-                };
-            })
-        });
+        data: convertData(item[1])
+    },
+    {
+        name: item[0] + ' Top10',
+        type: 'lines',
+        zlevel: 2,
+        symbol: ['none', 'arrow'],
+        symbolSize: 10,
+        effect: {
+            show: true,
+            period: 6,
+            trailLength: 0,
+            symbol: planePath,
+            symbolSize: 15
+        },
+        lineStyle: {
+            normal: {
+                color: color[i],
+                width: 1,
+                opacity: 0.6,
+                curveness: 0.2
+            }
+        },
+        data: convertData(item[1])
+    },
+    {
+        name: item[0] + ' Top10',
+        type: 'effectScatter',
+        coordinateSystem: 'geo',
+        zlevel: 2,
+        rippleEffect: {
+            brushType: 'stroke'
+        },
+        label: {
+            normal: {
+                show: true,
+                position: 'right',
+                formatter: '{b}'
+            }
+        },
+        symbolSize: function (val) {
+            return val[2] / 8;
+        },
+        itemStyle: {
+            normal: {
+                color: color[i]
+            }
+        },
+        data: item[1].map(function (dataItem) {
+            return {
+                name: dataItem[1].name,
+                value: geoCoordMap[dataItem[1].name].concat([dataItem[1].value])
+            };
+        })
+    });
 });
 
 var option = {
     backgroundColor: '#404a59',
     title : {
-        text: '仟金顶合同金额分布图',
+        text: '模拟迁徙',
         subtext: '数据纯属虚构',
         left: 'center',
         textStyle : {
-            color: '#000'
+            color: '#fff'
         }
     },
     tooltip : {
@@ -242,68 +268,30 @@ var option = {
         orient: 'vertical',
         top: 'bottom',
         left: 'right',
-        data:['杭州 Top10'],
+        data:['北京 Top10', '上海 Top10', '广州 Top10'],
         textStyle: {
             color: '#fff'
         },
         selectedMode: 'single'
     },
-    dataRange: {
-        min: 0,
-        max: 100,
-        x: 'right',
-        calculable: true,
-        color: ['#ff3333', 'orange', 'yellow', 'lime', 'aqua'],
-        textStyle: {
-            color: '#fff'
+    geo: {
+        map: 'china',
+        label: {
+            emphasis: {
+                show: false
+            }
+        },
+        roam: true,
+        itemStyle: {
+            normal: {
+                areaColor: '#323c48',
+                borderColor: '#404a59'
+            },
+            emphasis: {
+                areaColor: '#2a333d'
+            }
         }
     },
-    bmap: {
-        center: [107.741111, 33.166113],
-        zoom: 5,
-        roam: false,
-        mapStyle: {
-          styleJson: [
-          {
-                "featureType": "poi",
-                "elementType": "all",
-                "stylers": {
-                          "color": "#ffffff",
-                          "visibility": "off"
-                }
-              },
-                {
-                    "featureType": "label",
-                    "elementType": "all",
-                    "stylers": {
-                              "visibility": "off"
-                    }
-          			}
-            ]
-          }
-    },
-
-    // geo: {
-    //     map: 'world',
-    //     center: [115.97, 29.71],
-    //     zoom: 5,
-    //     label: {
-    //         emphasis: {
-    //             show: false
-    //         }
-    //     },
-    //     roam: true,
-    //     itemStyle: {
-    //         normal: {
-    //             areaColor: '#323c48',
-    //             borderColor: '#404a59'
-    //         },
-    //         emphasis: {
-    //             areaColor: '#2a333d'
-    //         }
-    //     }
-    // },
     series: series
 };
-
 export const options = option
